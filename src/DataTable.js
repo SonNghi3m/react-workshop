23936014d112247a884d94dd9329af9d430c5c9f
Son Nghiem <nghiemminhson@gmail.com>
2023-03-15 23:26:14 +0100

    Optional tasks are done.

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,20 +1,31 @@
 import React, { useEffect, useState } from 'react';
 
 const DataTable = () => {
+
+    const [student, setStudent] = useState({ id: '', firstName: '', lastName: '', age: '' });
+    const [showDetails, setShowDetails] = useState(false);
+
+    const handleShowDetails = (student) => {
+        setStudent(student);
+        setShowDetails(true);
+    }
+
+    const handleHideDetails = () => setShowDetails(false);
+
     return (
-        <div className='dataTable'>
+        <div className='dataTable border border-success border-3 rounded-3 m-2 '>
             <table className='table table-hover'>
                 <TableHeader />
-                <TableRow />
+                <TableRow onShowDetails={handleShowDetails} />
             </table>
+            {showDetails && <ShowStudentDetails student={student} onHideDetails={handleHideDetails} />}
         </div>
     );
 };
-export default DataTable;
 
 const TableHeader = () => {
     return (
-        <thead className='border-bottom border-2 border-dark'>
+        <thead className='border-bottom border-2 border-dark p-3 mb-2 bg-black text-white'>
             <tr>
                 <th>Id</th>
                 <th>First Name</th>
@@ -26,10 +37,10 @@ const TableHeader = () => {
     );
 }
 
-const TableAction = () => {
+const TableAction = ({ student, onShowDetails }) => {
     return (
         <div>
-            <button type='button' className='btn btn-info'>Details</button>
+            <button type='button' className='btn btn-success' onClick={() => onShowDetails(student)}>Details</button>
         </div>
     );
 }
@@ -60,7 +71,7 @@ const studentList = [
     }
 ];
 
-const TableRow = () => {
+const TableRow = ({ onShowDetails }) => {
     const [lists, setStudentList] = useState([{ id: '', firstName: '', lastName: '', age: '' }]);
     useEffect(() => {
         setStudentList(studentList);
@@ -75,7 +86,7 @@ const TableRow = () => {
                         <td>{student.firstName}</td>
                         <td>{student.lastName}</td>
                         <td>{student.age}</td>
-                        <td><TableAction /></td>
+                        <td><TableAction student={student} onShowDetails={onShowDetails} /></td>
                     </tr>
                 )
                 )
@@ -85,4 +96,16 @@ const TableRow = () => {
 
 }
 
-
+const ShowStudentDetails = ({ student, onHideDetails }) => {
+    return (
+        <div className='border border-success rounded-3 position-absolute top-50 start-50 translate-middle px-5 py-3'>
+            <h3>Student Information</h3>
+            <hr />
+            <p>Student Id: {student.id}</p>
+            <p>Full Name: {student.firstName} {student.lastName}</p>
+            <p>Age: {student.age}</p>
+            <button type='button' className='btn btn-success' onClick={onHideDetails}>Hide</button>
+        </div>
+    );
+};
+export default DataTable;
