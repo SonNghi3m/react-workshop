59c917a47fa9aeb23cb034727a7477eebc2942b9
Son Nghiem <nghiemminhson@gmail.com>
2023-03-16 09:17:20 +0100

    update to publish to git page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import ReactDOM from 'react-dom/client';
 import "bootstrap/dist/css/bootstrap.css";
 import DataTable from './DataTable';
 
-// const greetingMessage = <div>Welcome to React Workshop!</div>;
-// ReactDOM.createRoot(document.getElementById('root')).render(greetingMessage);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<DataTable />);
